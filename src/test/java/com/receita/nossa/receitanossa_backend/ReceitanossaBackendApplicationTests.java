27aac292584f54051e7c67475ed55e9f29d6c14d
makakof <luiz.h.s.silva@ufms.br>
2025-04-06 15:54:22 -0300

    commit inicial

---
+++ src/test/java/com/receita/nossa/receitanossa_backend/ReceitanossaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receita.nossa.receitanossa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitanossaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
